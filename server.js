e823ca6e0638bcfabe6eeadfd389775e3493ba32
pafad <32877302+pafad@users.noreply.github.com>
2018-06-17 18:38:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -98,6 +98,5 @@ client.on('ready', () => {
 client.on('message', msg => {
 	if (!msg.content.startsWith(tokens.prefix)) return;
 	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
-	audio(msg, client);
 });
 client.login(process.env.token);
